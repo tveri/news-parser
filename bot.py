4272421e44c71894365ba29a7e93bd3f71817a4b
tveri <rotro.volodya.tve@gamil.com>
2023-06-04 18:39:05 +0300

    add logger

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telebot import types
 
 import config, main
 
+
 bot = telebot.TeleBot(config.API_TOKEN)
 
 url = 'https://ria.ru/society/'
@@ -14,23 +15,23 @@ with open('./tmp', 'r+', encoding='utf-8') as f:
     lastWriting = int(lastPublicationTimestamp)
 
 while True:
-    print(format(datetime.datetime.now(), '%H:%M:%S  %D').center(100, '-'))
-    print('parsing...')
+    print(format(datetime.datetime.now(), '%H:%M:%S  %D').center(100, '-'), file=open(config.LOG_FILE_PATH, 'a', encoding='utf-8'))
+    print('parsing...', file=open(config.LOG_FILE_PATH, 'a', encoding='utf-8'))
     for news in main.parseNews(url, lastPublicationTimestamp):
         lastPublicationTimestamp = news['timestamp'] if news['timestamp'] > lastPublicationTimestamp else lastPublicationTimestamp
         post = f"<b>{news['content']['title']}</b>\n\n{news['content']['article-text']}"
         msg = bot.send_message(chat_id='@testnewsnews', text=post, parse_mode='HTML')
-        print(f"Post with title \"{news['content']['title']}\", message id: {msg.id}")
+        print(f"Post with title \"{news['content']['title']}\", message id: {msg.id}", file=open(config.LOG_FILE_PATH, 'a', encoding='utf-8'))
     else:
-        print('new posts weren\'t find')
+        print('new posts weren\'t find', file=open(config.LOG_FILE_PATH, 'a', encoding='utf-8'))
 
     if lastWriting != lastPublicationTimestamp:
         with open('./tmp', 'w', encoding='utf-8') as f:
             print(f'writing tmp with timestamp: {lastPublicationTimestamp}')
             f.write(str(lastPublicationTimestamp))
             lastWriting = int(lastPublicationTimestamp)
-    print(format(datetime.datetime.now(), '%H:%M:%S  %D').center(100, '-'))
+    print(format(datetime.datetime.now(), '%H:%M:%S  %D').center(100, '-'), file=open(config.LOG_FILE_PATH, 'a', encoding='utf-8'))
     time.sleep(sleepTime)
-    print(f'waiting {round(sleepTime/60, 1)} mins...')
+    print(f'waiting {round(sleepTime/60, 1)} mins...', file=open(config.LOG_FILE_PATH, 'a', encoding='utf-8'))
 
 # post = f"<b>{news['content']['title']}</b>\n\n{news['content']['article-text']}"
